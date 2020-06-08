bbf0ed49d1a0726c992e40aec6d0d3fcd00947b0
Vitaliy <Tkachenko_vitaliy@hotmail.com>
2020-06-08 13:15:34 +0300

    Update Listing-23.cpp

--- Listings/Listing-23.cpp
+++ Listings/Listing-23.cpp
@@ -1,12 +1,21 @@
-template<typename type, int size = 1>
+template<typename type, int size = 1>  // (1)
 class StaticArray
 {
 public:
     type array[size];
 };
 
+template <typename TYPE>  // (2)
+TYPE Sum(TYPE s2, TYPE s3)
+{
+    return s2 + s3;
+}
+
 int main()
 {
-    StaticArray<int, 1> someArray; //array consists of 1 item
-    //…
+    StaticArray<int, 1> someArray;  // (3)
+
+    int a = 0; double x = 8; 
+    Sum(a, a);  // (4)
+    Sum<double> (a, x);  // (5)
 }
