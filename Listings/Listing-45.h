56423886876c1689c07513f4df78bb537934de92
vitali tkachenka <Vitali_Tkachenka@epam.com>
2020-06-17 10:19:35 +0300

    Struct was changed for class

--- Listings/Listing-45.h
+++ Listings/Listing-45.h
@@ -2,39 +2,42 @@
 
 #include <memory>
 
-class UniArgument // (1)
+class UniArgument  // (1)
 {
 private:
-    struct Callable // (2)
+    class Callable  // (2)
     {
-        virtual void operator()(int) = 0; // (3)
+    public:
+        virtual void operator()(int) = 0;  // (3)
     };
 
-    std::unique_ptr<Callable> callablePointer; // (4)
+    std::unique_ptr<Callable> callablePointer;  // (4)
 
     template <typename ArgType>
-    struct CallableObject : Callable  // (5)
+    class CallableObject : public Callable  // (5)
     {
-        ArgType storedArgument; // (6)
+    public:
+        CallableObject(ArgType argument) : storedArgument(argument) { }  // (7)
 
-        CallableObject(ArgType argument) : storedArgument(argument) { } // (7)
-
-        void operator() (int value) override // (8)
+        void operator() (int value) override  // (8)
         {
-            storedArgument(value); // (9)
+            storedArgument(value);  // (9)
         }
+    private:
+        ArgType storedArgument;  // (6)
     };
 
 public:
-    void operator() (int value) // (10)
+    void operator() (int value)  // (10)
     {
-        callablePointer->operator()(value); // (11)
+        callablePointer->operator()(value);  // (11)
     }
 
     template <typename ArgType>
-    void operator = (ArgType argument) // (12)
+    void operator = (ArgType argument)  // (12)
     {
-        callablePointer.reset(new CallableObject<ArgType>(argument)); // (13)
+        callablePointer.reset(new CallableObject<ArgType>(argument));  // (13)
     }
 };
+
 
