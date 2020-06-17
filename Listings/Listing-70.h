c6607d1b7fea41c3198d31fd8f048942103fd144
Vitaliy <Tkachenko_vitaliy@hotmail.com>
2020-06-18 00:25:37 +0300

    Whitespaces

--- Listings/Listing-70.h
+++ Listings/Listing-70.h
@@ -2,19 +2,19 @@
 
 #include <tuple>
 
-template<std::size_t Index, typename CallObjects, typename CallData> // (1)
+template<std::size_t Index, typename CallObjects, typename CallData>            // (1)
 struct TupleIterator3
 {
     static void IterateTupleItem(CallObjects& callObjects, CallData& callData)  // (2)
     {
         const std::size_t idx = std::tuple_size_v<CallObjects> -Index;  // (3)
-        std::apply(std::get<idx>(callObjects), callData);  // (4)
+        std::apply(std::get<idx>(callObjects), callData);               // (4)
         TupleIterator3<Index - 1, CallObjects, CallData>::IterateTupleItem(callObjects, callData);  // (5)
     }
 };
 
 template<typename CallObjects, typename CallData>  // (6)
-struct TupleIterator3<0, CallObjects, CallData>  // (7)
+struct TupleIterator3<0, CallObjects, CallData>    // (7)
 {
     static void IterateTupleItem(CallObjects& callObjects, CallData& callData)  // (8)
     {
@@ -22,13 +22,13 @@ struct TupleIterator3<0, CallObjects, CallData>  // (7)
 };
 
 template<typename... CallObjects, typename... CallData>  // (9)
-void Distribute3(std::tuple<CallObjects...> callObjects, std::tuple<CallData...> callData) // (10)
+void Distribute3(std::tuple<CallObjects...> callObjects, std::tuple<CallData...> callData)  // (10)
 {
-    TupleIterator3  // (11) 
+    TupleIterator3                   // (11) 
         <
-        sizeof...(CallObjects),  // (12)
+        sizeof...(CallObjects),      // (12)
         std::tuple<CallObjects...>,  // (13)
-        std::tuple<CallData...>  // (14)
+        std::tuple<CallData...>      // (14)
         >
         ::IterateTupleItem(callObjects, callData);  // (15)
 }
