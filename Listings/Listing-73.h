975872b8e3ab75528bc908c48009299f618d3ec8
Vitaliy <Tkachenko_vitaliy@hotmail.com>
2020-06-20 07:07:17 +0300

    Whitespaces

--- Listings/Listing-73.h
+++ Listings/Listing-73.h
@@ -8,12 +8,12 @@ auto  DistributeReturnImpl(std::tuple<CallObjects...>& callObjects, std::index_s
     return std::tuple(std::get<indices>(callObjects)(callData...)...);  // (3)
 }
 
-template<typename... CallObjects, typename...CallData> // (4)
+template<typename... CallObjects, typename...CallData>                              // (4)
 auto DistributeReturn(std::tuple<CallObjects...> callObjects, CallData... callData) // (5)
 {
-    return DistributeReturnImpl(  // (6)
-        callObjects,  // (7)
-        std::make_index_sequence<sizeof...(CallObjects)>(),  // (8)
-        callData...); // (9)
+    return DistributeReturnImpl(                                                    // (6)
+        callObjects,                                                                // (7)
+        std::make_index_sequence<sizeof...(CallObjects)>(),                         // (8)
+        callData...);                                                               // (9)
 }
 
