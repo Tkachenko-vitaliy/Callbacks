cf7e8c049bf8247da20bd343ec2ef77d6d64ecd3
Vitaliy <Tkachenko_vitaliy@hotmail.com>
2021-01-12 01:28:14 +0300

    Update Listing-4.cpp

--- Listings/Listing-4.cpp
+++ Listings/Listing-4.cpp
@@ -1,20 +1,21 @@
 #include "Listing-2.h"
 
-struct ÑontextData // (1)
+struct ContextData // (1)
 {
     //some context data
 };
 
-void callbackHandler(int eventID, void* somePointer) // (2) 
+void callbackHandler(int eventID, void* somePointer)                    // (2) 
 {
     //It will be called by initiator
-    ÑontextData* pContextData = static_cast<ÑontextData*>(somePointer); // (3) cast to context
+    ContextData* pContextData = static_cast<Ã‘ontextData*>(somePointer); // (3) cast to context
+    //Do something here
 }
 
 int main() // (4)
 {
-    Initiator  initiator;  // (5)
-    ÑontextData clientContext; // (6)
+    Initiator  initiator;                             // (5)
+    Ã‘ontextData clientContext;                        // (6)
     initiator.setup(callbackHandler, &clientContext); // (7) callback setup 
     initiator.run(); // (8) initiator has been run
     //Wait finish
